22f7cc0a9f6ec16e0fef92ad3f1e3148b0a8ed96
Daniil Ponomarev <daniiil.ponomarev@gmail.com>
2019-03-21 20:10:10 +0300

    Update grid-styles to @rebass/grid, fix incompatibility

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,44 +1,51 @@
-import React, { StrictMode } from 'react'
-import { Redirect } from 'react-router'
-import { Router, Route, Switch } from 'react-router-dom'
-// import styled, { injectGlobal, ThemeProvider } from 'styled-components'
-import styled, { ThemeProvider } from 'styled-components'
-import { Flex } from 'grid-styled'
-import { map, values } from 'ramda'
-import { createBrowserHistory } from 'history'
+import React, { StrictMode } from 'react';
+import { Redirect } from 'react-router';
+import { Router, Route, Switch } from 'react-router-dom';
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
+// import styled, { ThemeProvider } from 'styled-components'
+import { Flex } from '@rebass/grid';
+import { map, values } from 'ramda';
+import { createBrowserHistory } from 'history';
 
-import { Container, Container2 } from './containers'
-import { ErrorBoundary, PageHeader, PageFooter } from './components'
-import './App.css'
-import { colors, sizes, routes } from './common'
+import { Container, Container2 } from './containers';
+import { ErrorBoundary, PageHeader, PageFooter } from './components';
+import './App.css';
+import { colors, sizes, routes } from './common';
 
-// injectGlobal`
-//   html {
-//     font-size: .625rem;
-//     font-family: Tahoma, Arial, sans-serif;
-//     -webkit-font-smoothing: antialiased;
-//   }
-//   html, body {
-//     margin: 0;
-//     padding: 0;
-//     height: 100%;
-//     background: ${colors.white};
-//   }
-//
-//   input, textarea, select, button {
-//     font-family: Tahoma, Arial, sans-serif;
-//   }
-//
-//   #root {
-//     height: 100%;
-//   }
-// `
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: .625rem;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 
+                  'Droid Sans', 'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  html, body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    background: ${colors.white};
+  }
 
-const history = createBrowserHistory()
+  input, textarea, select, button {
+    font-family: Tahoma, Arial, sans-serif;
+  }
+
+  #root {
+    height: 100%;
+  }
+  
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+  }
+`;
+
+const history = createBrowserHistory();
 
 const theme = {
   breakpoints: map(item => `${item}em`, values(sizes)),
-}
+};
 
 history.listen((location, action) =>
   setTimeout(() => {
@@ -48,24 +55,25 @@ history.listen((location, action) =>
     // - manually changed the URL in the address bar (here we might want
     // to scroll to top, but we can't differentiate it from the others)
     if (action === 'POP' || action === 'PUSH' || (location.state && location.state.noScroll)) {
-      return
+      return;
     }
 
     // In all other cases, scroll to top
-    window.scrollTo(0, 0)
+    window.scrollTo(0, 0);
   }),
-)
+);
 
 const AppWrapper = styled.div`
   height: 100%;
-  max-width: 1366px;
-`
+  // max-width: 1366px;
+`;
 
 const App = () => (
   <StrictMode>
+    <GlobalStyle whiteColor />
     <ThemeProvider theme={theme}>
       <Router history={history}>
-        <Flex is={AppWrapper} flexDirection="column" m="0 auto">
+        <Flex as={AppWrapper} flexDirection="column" m="0 auto">
           <PageHeader />
           <Flex mt="7rem" flex="1 0 auto" flexDirection="column">
             <ErrorBoundary>
@@ -82,6 +90,6 @@ const App = () => (
       </Router>
     </ThemeProvider>
   </StrictMode>
-)
+);
 
-export default App
+export default App;
--- front/src/containers/container2/container2.jsx
+++ front/src/containers/container2/container2.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-import { Spinner } from '../../components'
-import logo from '../../logo.svg'
+import { Spinner } from '../../components';
+import logo from '../../logo.svg';
 
 export class Container2 extends React.Component {
   render() {
@@ -14,6 +14,6 @@ export class Container2 extends React.Component {
         2
         <Spinner />
       </div>
-    )
+    );
   }
 }
