55ce2e571a6802d017e4bd4432efc99e152e953b
Daniil Ponomarev <daniiil.ponomarev@gmail.com>
2019-04-14 16:04:41 +0300

    Fix routing

--- src/main/java/com/diploma/DiplomaApplication.java
+++ src/main/java/com/diploma/DiplomaApplication.java
@@ -2,6 +2,8 @@ package com.diploma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DiplomaApplication {
@@ -10,4 +12,12 @@ public class DiplomaApplication {
         SpringApplication.run(DiplomaApplication.class, args);
     }
 
+    @Controller
+    public class ViewController {
+
+        @RequestMapping({"general", "container1", "container2",})
+        public String index() {
+            return "forward:/index.html";
+        }
+    }
 }
